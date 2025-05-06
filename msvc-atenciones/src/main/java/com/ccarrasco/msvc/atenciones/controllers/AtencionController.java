04d8979cd5dbf10c04e2d36605ccaf8f2df9c903
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-05-06 11:43:28 -0400

    Se genera el msvc atencion

---
+++ msvc-atenciones/src/main/java/com/ccarrasco/msvc/atenciones/controllers/AtencionController.java
@@ -0,0 +1,36 @@
+package com.ccarrasco.msvc.atenciones.controllers;
+
+import com.ccarrasco.msvc.atenciones.models.Atencion;
+import com.ccarrasco.msvc.atenciones.services.AtencionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/atenciones")
+@Validated
+public class AtencionController {
+
+    @Autowired
+    private AtencionService atencionService;
+
+    @GetMapping
+    public ResponseEntity<List<Atencion>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.atencionService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Atencion> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.atencionService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Atencion> save(@RequestBody @Valid Atencion atencion) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.atencionService.save(atencion));
+    }
+}
