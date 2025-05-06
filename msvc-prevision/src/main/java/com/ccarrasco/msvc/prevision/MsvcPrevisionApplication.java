de4c932234a96e695a009eac560e86ff1811e09b
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-05-05 22:18:55 -0400

    Se carga el msvc de prevision

---
+++ msvc-prevision/src/main/java/com/ccarrasco/msvc/prevision/MsvcPrevisionApplication.java
@@ -0,0 +1,13 @@
+package com.ccarrasco.msvc.prevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPrevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPrevisionApplication.class, args);
+	}
+
+}
---
+++ msvc-prevision/src/test/java/com/ccarrasco/msvc/prevision/MsvcPrevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarrasco.msvc.prevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPrevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
