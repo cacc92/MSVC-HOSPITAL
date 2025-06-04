7c613b6989770eb63d10c17f8216568117a33a76
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-06-03 23:57:36 -0400

    Se genera la auto carga con data fake

--- msvc-medicos/src/main/java/com/ccarrasco/msvc/medicos/init/LoadDatabase.java
+++ msvc-medicos/src/main/java/com/ccarrasco/msvc/medicos/init/LoadDatabase.java
@@ -2,27 +2,51 @@ package com.ccarrasco.msvc.medicos.init;
 
 import com.ccarrasco.msvc.medicos.models.entities.Medico;
 import com.ccarrasco.msvc.medicos.repositories.MedicoRepository;
+import net.datafaker.Faker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class LoadDatabase {
+import java.util.Locale;
+import java.util.Random;
+
+@Profile("dev")
+@Component
+public class LoadDatabase implements CommandLineRunner {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(MedicoRepository medicoRepository) {
-        return args -> {
-            if(medicoRepository.count() == 0) {
-                Medico cesar = new Medico("18201315-3","César Carrasco","Pediatra");
-                Medico alexander = new Medico("18201315-1","Alexander Carré","Oncologo");
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker(Locale.of("es", "CL"));
+        Random random = new Random();
 
-                log.info("Carga incial: {}", medicoRepository.save(cesar));
-                log.info("Carga incial: {}", medicoRepository.save(alexander));
+        // en caso que no exista nada se crea el elemento
+        if(medicoRepository.count() == 0) {
+
+            for (int i = 0; i < 30; i++) {
+                Medico medico = new Medico();
+
+                // Con esto genero un numero fake de un rut
+                String numeroStr = faker.idNumber().valid().replaceAll("-", "");
+                String ultimo = numeroStr.substring(numeroStr.length() - 1);
+                String restante = numeroStr.substring(0, numeroStr.length() - 1);
+
+                // Se setean todas las propiedades
+                medico.setRunMedico(restante + "-" + ultimo);
+                medico.setEspecialidad(faker.careProvider().medicalProfession());
+                medico.setNombreCompleto(faker.name().fullName());
+                medico = medicoRepository.save(medico);
+
+                log.info("Medico creado {}", medico);
+
             }
-        };
+        }
     }
 }
